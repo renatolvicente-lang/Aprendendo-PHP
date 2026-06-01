39637f6721e435a3efe5b68a2e471f267646015b
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-06-01 14:39:31 -0300

    doc - da página de logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,8 @@
 <?php
-
-    session_start();
-    session_destroy();
-    header("Location: ../index.php");
+    //ativa quando o usuario aperta no botão de sair na pagina home.php
+    session_start();//starta a sessão do usuario
+    session_destroy();//"Destroi" a sessão do usuario
+    header("Location: ../index.php");//leva para a página index.php
     exit();
 
 ?>
