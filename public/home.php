0264790c4e7d88fe070eb335b940d947efc6aaf4
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-06-01 14:47:40 -0300

    doc - documentação da pagina home.php

--- public/home.php
+++ public/home.php
@@ -1,23 +1,23 @@
 <?php
-session_start();
-if(!isset($_SESSION["usuario"])){
-    header("Location: ../index.php");
+session_start();// inicia a sessão do usuário
+if(!isset($_SESSION["usuario"])){// verifica se já ouve um login verificando se há uma sessão com nome adicionado na variavel usuario
+    header("Location: ../index.php");//volta para a página index.php
     exit();
 }
 
-include("../infra/db/connect.php");
+include("../infra/db/connect.php");//inclue um componente que connecta com o BD
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $novoUsuario = $_POST['usuario'];
-    $novaSenha = $_POST['senha'];
+if($_SERVER["REQUEST_METHOD"] == "POST"){//verifica se o metodo de request é "POST"
+    $novoUsuario = $_POST['usuario'];// armazena os dados inseridos no input de name "usuario" da tabela de cadastro
+    $novaSenha = $_POST['senha'];// armazena os dados inseridos no input de name "senha" da tabela de cadastro
 
     $sql = "INSERT INTO usuarios (usuario,senha) 
-    VALUES ('$novoUsuario','$novaSenha')";  
+    VALUES ('$novoUsuario','$novaSenha')";  // armazena a query que insere novos valores na tabela do BD
 
-    if($conn->query($sql) === TRUE){
-        echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
+    if($conn->query($sql) === TRUE){//verifica se a query foi criada
+        echo "<script> alert('Usuário cadastrado com sucesso!')</script>";// mensagem de sucesso
     }else{
-        echo "<script> alert('Erro ao cadastrar')</script>";
+        echo "<script> alert('Erro ao cadastrar')</script>";//mensagem de erro
     }
 
 };
@@ -31,7 +31,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Home</title>
 </head>
 <body>
+    <!-- h3 serve para exibir um titulo com uma mensagem e exibir o nome inserido anteriormente -->
     <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
+    <!-- Botão que executa a função da página logout.php -->
     <a href="logout.php"> Sair</a>
 
     <hr>
@@ -46,7 +48,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <?php
         
             if(isset($erro)){
-                echo $erro;
+                echo $erro;// insere mensagem de erro 
             };
         
         ?>
