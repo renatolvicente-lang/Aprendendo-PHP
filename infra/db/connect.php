172a20dbfa2ac72a96151186b1a17b1547afc517
renatolvicente-lang <renato_l_vicente@estudante.sesisenai.org.br>
2026-06-03 09:59:49 -0300

    doc - connect.php

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,16 +1,16 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db = "sistema_simples_m1";
+    $host = "localhost";//Variavel que recebe o valor de Host
+    $user = "root";//Variavel que recebe o valor de root
+    $pass = "root";//Variavel que recebe o valor de root
+    $db = "sistema_simples_m1";//Variavel que recebe o valor de sistema_simples_m1
 
-    $conn = new mysqli($host,$user,$pass,$db);
+    $conn = new mysqli($host,$user,$pass,$db);//cria uma conexão com o BD usando os dados das variaveis anteriores.
 
-    if($conn->connect_error){
-        die("Erro na conexão!");
+    if($conn->connect_error){//verifica se ouve erro de conexão
+        die("Erro na conexão!");//caso de erro na conexão
     }else{
-        echo "<script>console.log('Banco conectado com sucesso!')</script>";
+        echo "<script>console.log('Banco conectado com sucesso!')</script>";//caso de certo a conexão
     };
 
 ?>
