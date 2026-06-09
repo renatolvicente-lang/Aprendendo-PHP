6fb7b6146ebb93de6f69e47a6958b55e253144a4
Renato L. Vicente <renato_l_vicente@estudante.sesisenai.org.br>
2026-06-09 16:55:40 -0300

    doc - script.sql

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE sistema_simples_m1;
+CREATE DATABASE sistema_simples_m1; --cria o Banco de dados
 
-USE sistema_simples_m1;
+USE sistema_simples_m1;--chama o banco de dados
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE usuarios (--cria uma tabela
+    id INT AUTO_INCREMENT PRIMARY KEY,--colunas da tabela
     usuario VARCHAR(87) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
 
-INSERT INTO usuarios (usuario, senha) VALUES ('admin','123');
+INSERT INTO usuarios (usuario, senha) VALUES ('admin','123');--insere um valor na tabela
